86581adb50df4bc51bf42e2346d034115178971d
Timilehin <aregbestimi@gmail.com>
2020-08-23 01:26:04 +0100

    added videoGrid

--- public/script.js
+++ public/script.js
@@ -1,7 +1,17 @@
 const socket = io('/')
+const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
     host: '/',
     port: '3001'
+})
+const myVideo = document.createElement('video')
+myVideo.muted = true
+
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then(stream => {
+    addVideoStream(myVideo, stream)
 })
 
 myPeer.on('open', id => {
@@ -12,3 +22,11 @@ myPeer.on('open', id => {
 socket.on('user-connected', userId => {
     console.log('User connected: ' + userId)
 })
+
+function addVideoStream(video, stream) {
+    video.srcObject = stream
+    video.addEventListener('loadedmetadata', () => {
+        video.play()
+    })
+    videoGrid.append(video)
+}
