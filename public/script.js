e0297d68cd75deac02eb1139056d1c5efe504273
Timilehin <aregbestimi@gmail.com>
2020-08-28 15:08:35 +0100

    port

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
   host: 'nameless-oasis-80255.herokuapp.com',
-  port: '443'
+  port: '3000'
 })
 const myVideo = document.createElement('video')
 myVideo.muted = true
