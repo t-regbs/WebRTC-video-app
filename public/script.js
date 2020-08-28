f5151abb2b496bb510eb143226a494856d6703ee
Timilehin <aregbestimi@gmail.com>
2020-08-28 13:41:58 +0100

    try peer fix

--- public/script.js
+++ public/script.js
@@ -1,10 +1,6 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
-const myPeer = new Peer(undefined, {
-  host: 'nameless-oasis-80255.herokuapp.com',
-  port: '3001',
-  'path' : '/peerjs'
-})
+const myPeer = new Peer()
 const myVideo = document.createElement('video')
 myVideo.muted = true
 const peers = {}
